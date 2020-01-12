69e7248ef0bd5b93eae1a812a311015ab7a9b2f8
Victor Sued <visued@gmail.com>
2020-01-11 23:32:48 -0300

    Rotas: Definindo e extraindo parâmetros de roteamento

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 const APP_ROUTES: Routes  = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'cursos', component: CursosComponent },
+    { path: 'cursos/', component: CursosComponent },
+    { path: 'cursos/:id', component: CursoDetalheComponent },
 
 ];
 

d968f89f4f95bbb06ac18166822eb168a329ab0f
Victor Sued <visued@gmail.com>
2020-01-31 18:36:41 -0300

    Estilo de url: HTML5 ou usando #

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -16,8 +16,9 @@ import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao
 const appRoutes: Routes  = [
     { path: 'cursos', loadChildren: 'src/app/cursos/cursos.module#CursosModule', canActivate: [AuthGuard], canActivateChild: [CursosGuard], canLoad: [AuthGuard]},
     { path: 'alunos', loadChildren: 'src/app/alunos/alunos.module#AlunosModule', canActivate: [AuthGuard], canActivateChild: [AlunosGuard], canLoad: [AuthGuard]},
-    { path: '', component: HomeComponent, canActivate: [AuthGuard]},
     { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: '**', component: PaginaNaoEncontradaComponent },
 
     // { path: 'cursos', component: CursosComponent },
@@ -30,7 +31,7 @@ const appRoutes: Routes  = [
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, {useHash: true})
   ],
   exports: [RouterModule]
 })
